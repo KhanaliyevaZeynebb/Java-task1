b216221a388469d49aa6c5dbc49e0cc7286c80f6
Zeyneb <x.zeynebb@gmail.com>
2026-09-23 17:59:23 +0400

    Task2

---
+++ src/Task2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("Cəm:"+(a+b));
+        System.out.println("Fərq:"+(a-b));
+        System.out.println("Hasil:"+(a*b));
+        System.out.println("Bölmə:"+(a/b));
+        System.out.println("Qalıq:"+(a%b));
+
+
+    }
+}
