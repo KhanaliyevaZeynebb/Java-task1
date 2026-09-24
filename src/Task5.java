0d3966fe84626d9672d5a33fbb7adae010875e8f
Zeyneb <x.zeynebb@gmail.com>
2026-09-24 14:18:42 +0400

    Task5

---
+++ src/Task5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Task5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        if (a %2==0) {
+            System.out.println("Cüt");
+        } else {
+            System.out.println("Tek");
+
+
+            }
+        }
+
+    }
+
